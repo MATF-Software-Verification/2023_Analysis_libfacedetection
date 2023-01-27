fe4e4f01488e9becc1ffe3605b62676becc502f9
MarijaEric <marija.eric1997@gmail.com>
2023-01-27 18:15:30 +0100

    Added output for GCov, Memcheck and Massif.

---
+++ Massif/run_massif.sh
@@ -0,0 +1,1 @@
+valgrind --tool=massif --stacks=yes --massif-out-file="massif-detect-camera.txt" ./detect-camera 0
---
+++ Memcheck/run_memcheck.sh
@@ -0,0 +1,1 @@
+valgrind -s --leak-check=full --log-file="memcheck-detect-image.txt" ./detect-image ../TestSamples/image-test.jpg
